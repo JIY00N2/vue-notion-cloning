3983970c51bebb5cee89882071a187fdaeef80f2
JiYoon <angella990825@gmail.com>
2023-08-06 14:29:06 +0900

    Add router

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './Home';
+
+export default createRouter({
+  history: createWebHistory(),
+  scrollBehavior: () => ({ top: 0 }),
+  routers: [
+    {
+      path: '/',
+      component: Home,
+    },
+  ],
+});
+
+// 페이지가 바뀔 때마다 스크롤 최상단 위치
